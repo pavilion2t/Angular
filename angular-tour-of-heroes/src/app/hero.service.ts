8921048fe7b7de2d59c8ea32c0b9973c5d0c1e47
Judy <judy.zhu@bindo.com>
2018-03-23 16:20:14 +0800

    ng generate service message --module=app

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,13 +5,15 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
+    // todo: send the message after fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
