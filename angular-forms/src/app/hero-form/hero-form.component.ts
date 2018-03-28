00bf22dbc979fd1dc1051158d7204a82ecf767cb
Judy <judy.zhu@bindo.com>
2018-03-28 23:08:06 +0800

    angular-form

---
+++ angular-forms/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
