a134469a5ae34e733bebf630789adafd715c5f27
Judy Zhu <850832627@qq.com>
2018-07-28 15:28:57 +0800

    app.module.ts

--- Auction/src/app/app.module.ts
+++ Auction/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ //声明模块中有什么，只能声明组件指令和管道
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [//
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //只能声明服务，依赖注入
+  bootstrap: [AppComponent] //声明模块的主组件
 })
 export class AppModule { }
+
+//模块，一个带着装饰器的typescript类
