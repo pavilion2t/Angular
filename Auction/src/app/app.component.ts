a3fcb9877549c02959bc8464e5fa8637c8ff5755
Judy Zhu <850832627@qq.com>
2018-07-28 15:23:47 +0800

    explain

--- Auction/src/app/app.component.ts
+++ Auction/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// 装饰器 模板 控制器
+// 所有组件都使用装饰器来注入元数据，把他们加到AppComponent类
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // 可以通过这个html标签来调用组件内容
+  templateUrl: './app.component.html', // 组件内容，模板
+  styleUrls: ['./app.component.css']  
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {  // 定义控制器，页面的逻辑
+  title = 'Judy';
 }
+
+// 输入属性@Inputs() 提供器providers 生命周期钩子 
